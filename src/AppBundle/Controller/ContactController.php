1aa37e73fb879c4a92367a5201c71c5a97b06b1c
Mathieu YamiShadow <mathieu.nibas@gmail.com>
2018-05-02 16:14:36 +0200

    maj contact form

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -6,7 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use AppBundle\Entity\Contact;
 /***
  * Class ContactController
  * @package AppBundle\Controller
@@ -22,8 +22,26 @@ class ContactController extends Controller
      * )
      * @return Response
      */
-    public function contact(){
-        return $this->render('contact/contact.html.twig');
+    public function contact(Request $request){
+
+        $contact = new Contact();
+        dump($contact);
+
+
+        $form = $this->createFormBuilder($contact)
+            ->add('email')
+            ->add('subject')
+            ->add('message')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            dump($form->getData());
+        }
+        return $this->render('contact/contact.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
 }
---
+++ src/AppBundle/Entity/Contact.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shado
+ * Date: 02/05/2018
+ * Time: 15:50
+ */
+
+namespace AppBundle\Entity;
+
+
+class Contact
+{
+
+    private $subject;
+    private $email;
+    private $message;
+
+    /**
+     * @return mixed
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param mixed $subject
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+    private $message;
+
+}
